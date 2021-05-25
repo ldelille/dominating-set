1ed21686e0ea40e446b54dd75c44aaac9af8e135
Delille Louis <louis.delille@supelec.fr>
2021-05-25 21:18:17 +0200

    cleaned imports, reformated code

--- Algorithms/dominatingKCenters.py
+++ Algorithms/dominatingKCenters.py
@@ -1,36 +1,33 @@
 import sys, os, time
 import networkx as nx
-import sys, os, time
-import networkx as nx
 from networkx.utils import arbitrary_element
 
 
-#Given a graph G and a integer i, we find i centers that minimize the maximum distance of a node to one center 
-#Returns the i centers and the max distance
+# Given a graph G and a integer i, we find i centers that minimize the maximum distance of a node to one center
+# Returns the i centers and the max distance
 def findKCenters(G, k, shortestPaths):
     allNodes = set(G)
     kCenters = set()
     start_with = arbitrary_element(allNodes)
     kCenters.add(start_with)
     nodeDict = {}
-    for x in allNodes: 
+    for x in allNodes:
         nodeDict[x] = float("inf")
     k = k - 1
     nodeDict[start_with] = 0
-    while k != 0: 
-        for node in allNodes-kCenters:
+    while k != 0:
+        for node in allNodes - kCenters:
             for center in kCenters:
-                if nodeDict[node] > shortestPaths[node][center]: 
+                if nodeDict[node] > shortestPaths[node][center]:
                     nodeDict[node] = shortestPaths[node][center]
-        newCenter = max(nodeDict, key = lambda i:nodeDict[i])
+        newCenter = max(nodeDict, key=lambda i: nodeDict[i])
         nodeDict[newCenter] = 0
         kCenters.add(newCenter)
-        k -=1
+        k -= 1
     return kCenters, max(nodeDict.values())
 
 
-
-#for each graph, we calculate the min distance for i centers, if the min distance is equal to 1, we found a dominating set
+# for each graph, we calculate the min distance for i centers, if the min distance is equal to 1, we found a dominating set
 def dominant(g):
     n = len(g.nodes)
     shortestPaths = dict(nx.all_pairs_shortest_path_length(g))
@@ -40,35 +37,28 @@ def dominant(g):
             return kCenters
 
 
-
-
-
-
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     input_dir = os.path.abspath(sys.argv[1])
     output_dir = os.path.abspath(sys.argv[2])
-    
+
     # input dir
     if not os.path.isdir(input_dir):
-	    print(input_dir, "doesn't exist")
-	    exit()
+        print(input_dir, "doesn't exist")
+        exit()
 
     # output dir
     if not os.path.isdir(output_dir):
-	    print(input_dir, "doesn't exist")
-	    exit()       
-	
-    # response file
-    output_filename = 'answers_{}.txt'.format(time.strftime("%d%b%Y_%H%M%S", time.localtime()))             
+        print(input_dir, "doesn't exist")
+        exit()
+
+        # response file
+    output_filename = 'answers_{}.txt'.format(time.strftime("%d%b%Y_%H%M%S", time.localtime()))
     output_file = open(os.path.join(output_dir, output_filename), 'w')
 
     for graph_filename in sorted(os.listdir(input_dir)):
         # importe graph
         g = nx.read_adjlist(os.path.join(input_dir, graph_filename))
-        
+
         # calculate dominating graph
         D = sorted(dominant(g), key=lambda x: int(x))
         # add to response
@@ -76,5 +66,5 @@ if __name__=="__main__":
         for node in D:
             output_file.write(' {}'.format(node))
         output_file.write('\n')
-        
+
     output_file.close()
--- Algorithms/dominatingKCentersOptimized.py
+++ Algorithms/dominatingKCentersOptimized.py
@@ -10,27 +10,22 @@ from random import randrange
 import operator
 
 
-
 def findBestNextColor(g, totalNodes, blackNodes, whiteNodes, currentDominating):
     maxNeighborhood = 0
     candidate = None
-    for x in totalNodes-currentDominating:
+    for x in totalNodes - currentDominating:
         currentLen = len(set(g[x]).intersection(blackNodes))
-        if currentLen > maxNeighborhood: 
+        if currentLen > maxNeighborhood:
             candidate = x
             maxNeighborhood = currentLen
     return candidate or totalNodes.pop()
-
-
-
-
 
 
 def findKCentersColors(G, k, shortestPaths, startWith):
     allNodes = set(G)
     kCenters = set()
     nodeDict = {}
-    for x in allNodes: 
+    for x in allNodes:
         nodeDict[x] = float("inf")
     k = k - 1
     nodeDict[startWith] = 0
@@ -45,27 +40,28 @@ def findKCentersColors(G, k, shortestPaths, startWith):
     for x in set(g[newCenter]):
         whiteNodes.add(x)
     while k != 0:
-        for node in allNodes-kCenters:
-            if nodeDict[node] > shortestPaths[node][newCenter]: 
+        for node in allNodes - kCenters:
+            if nodeDict[node] > shortestPaths[node][newCenter]:
                 nodeDict[node] = shortestPaths[node][newCenter]
         newCenter = findBestNextColor(g, allNodes, blackNodes, whiteNodes, kCenters)
-        if not newCenter: 
+        if not newCenter:
             return
-        blackNodes -= set(g[newCenter]) 
+        blackNodes -= set(g[newCenter])
         blackNodes -= {newCenter}
         for x in set(g[newCenter]):
             whiteNodes.add(x)
         whiteNodes.add(newCenter)
         nodeDict[newCenter] = 0
         kCenters.add(newCenter)
-        k = k-1
+        k = k - 1
     return kCenters, max(nodeDict.values())
+
 
 def findKCentersColorsEdgeNodes(G, k, shortestPaths, edgeNodes):
     allNodes = set(G)
     kCenters = set()
     nodeDict = {}
-    for x in allNodes: 
+    for x in allNodes:
         nodeDict[x] = float("inf")
     k = k - 1
     blackNodes = set(G)
@@ -79,18 +75,18 @@ def findKCentersColorsEdgeNodes(G, k, shortestPaths, edgeNodes):
             whiteNodes.add(x)
         kCenters.add(y)
         nodeDict[y] = 0
-    for node in allNodes-kCenters:
+    for node in allNodes - kCenters:
         for center in kCenters:
-            if nodeDict[node] > shortestPaths[node][center]: 
+            if nodeDict[node] > shortestPaths[node][center]:
                 nodeDict[node] = shortestPaths[node][center]
 
     k = k - len(edgeNodes)
     while k != 0:
         newCenter = findBestNextColor(g, allNodes, blackNodes, whiteNodes, kCenters)
-        if not newCenter: 
+        if not newCenter:
             return
-        for node in allNodes-kCenters:
-            if nodeDict[node] > shortestPaths[node][newCenter]: 
+        for node in allNodes - kCenters:
+            if nodeDict[node] > shortestPaths[node][newCenter]:
                 nodeDict[node] = shortestPaths[node][newCenter]
         blackNodes -= set(g[newCenter])
         blackNodes -= {newCenter}
@@ -99,36 +95,33 @@ def findKCentersColorsEdgeNodes(G, k, shortestPaths, edgeNodes):
         whiteNodes.add(newCenter)
         nodeDict[newCenter] = 0
         kCenters.add(newCenter)
-        k = k-1
-        print("allNodes", allNodes)
-        print("kcenters", kCenters)
-        print(nodeDict)
+        k = k - 1
     return kCenters, max(nodeDict.values())
 
 
-def getNodeWithMostNeighbors(g): 
-    allNodes = set(g) 
-    startWith =  None
+def getNodeWithMostNeighbors(g):
+    allNodes = set(g)
+    startWith = None
     maxNeighborhood = 0
-    for x in allNodes: 
+    for x in allNodes:
         if len(g[x]) > maxNeighborhood:
             startWith = x
             maxNeighborhood = len(g[x])
     return startWith
 
+
 def getRandomNode(g):
-    allNodes = set(g) 
+    allNodes = set(g)
     n = len(g.nodes)
     l = randrange(n)
     j = 0
-    startWith =  None
-    for x in allNodes: 
+    startWith = None
+    for x in allNodes:
         if j == l:
             startWith = x
             break
-        j = j+1
+        j = j + 1
     return startWith
-
 
 
 def orderNodesByDegree(g, i):
@@ -137,12 +130,13 @@ def orderNodesByDegree(g, i):
     nodes = [node for (node, val) in g.degree() if val == minimum]
     return nodes
 
-def getEdgeNodes(g): 
+
+def getEdgeNodes(g):
     allNodes = set(g)
     edgeNodes = set()
     for y in allNodes:
-        if len(g[y])==1:
-            for x in g[y]: 
+        if len(g[y]) == 1:
+            for x in g[y]:
                 edgeNodes.add(x)
     return edgeNodes or getRandomNode(g)
 
@@ -155,11 +149,8 @@ def callKcenters(g):
         if d == 1:
             print("findKCentersColors MOST NEIGHBORS")
             return kCenters
-  
-
 
         # iterations = 10
-
 
 
 def dominant(g):
@@ -170,7 +161,7 @@ def dominant(g):
     blackNodes = totalNodes
     whiteNodes = set()
     while blackNodes:
-        v = findBestNextColor(g, totalNodes, blackNodes, whiteNodes,currentDominatingColors)
+        v = findBestNextColor(g, totalNodes, blackNodes, whiteNodes, currentDominatingColors)
         currentDominatingColors.add(v)
         blackNodes -= set(g[v])
         blackNodes -= {v}
@@ -178,45 +169,37 @@ def dominant(g):
             whiteNodes.add(x)
         whiteNodes.add(v)
 
-
-    if len(currentDominatingColors) < bestLenght: 
+    if len(currentDominatingColors) < bestLenght:
         bestDominating = currentDominatingColors
         bestLenght = len(bestDominating)
         # print("choose Colors")
-
-    
-
 
     # print(nx.is_dominating_set(g, bestDominating))
     return bestDominating
 
 
-
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     input_dir = os.path.abspath(sys.argv[1])
     output_dir = os.path.abspath(sys.argv[2])
-    
+
     # input dir
     if not os.path.isdir(input_dir):
-	    print(input_dir, "doesn't exist")
-	    exit()
+        print(input_dir, "doesn't exist")
+        exit()
 
     # output dir
     if not os.path.isdir(output_dir):
-	    print(input_dir, "doesn't exist")
-	    exit()       
-	
-    # response file
-    output_filename = 'answers_{}.txt'.format(time.strftime("%d%b%Y_%H%M%S", time.localtime()))             
+        print(input_dir, "doesn't exist")
+        exit()
+
+        # response file
+    output_filename = 'answers_{}.txt'.format(time.strftime("%d%b%Y_%H%M%S", time.localtime()))
     output_file = open(os.path.join(output_dir, output_filename), 'w')
 
     for graph_filename in sorted(os.listdir(input_dir)):
         # importe graph
         g = nx.read_adjlist(os.path.join(input_dir, graph_filename))
-        
+
         # calculate dominating graph
         D = sorted(dominant(g), key=lambda x: int(x))
         # add to response
@@ -224,5 +207,5 @@ if __name__=="__main__":
         for node in D:
             output_file.write(' {}'.format(node))
         output_file.write('\n')
-        
+
     output_file.close()
